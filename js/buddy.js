56bea1d38fb5a2d7bf61ce1cc16d96188d279e61
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-22 21:05:24 +0600

    (advanced) Handle and display Nested API data

---
+++ js/buddy.js
@@ -0,0 +1,20 @@
+
+// load data from randomuser
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddies(data));
+}
+loadBuddies();
+
+// display data on UI
+const displayBuddies = data => {
+    const buddies = data.results;
+    const buddiesDiv = document.getElementById('buddies');
+    for(const buddy of buddies){
+        // console.log(buddy.name.first, buddy.name.last);
+        const p = document.createElement('p');
+        p.innerText = `Name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last} Email: ${buddy.email}`;
+        buddiesDiv.appendChild(p);
+    }
+}
