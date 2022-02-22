11efb601ffea640104cd81ad4c7383aa7551b402
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-22 22:07:01 +0600

    International Travel, Display countries name, capital

---
+++ js/countries.js
@@ -0,0 +1,29 @@
+
+// load countries data
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+loadCountries();
+
+// display countries
+const displayCountries = countries => {
+    /* for(const country of countries){
+        console.log(country);
+    } */
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.classList.add('country');
+        const h3 = document.createElement('h3');
+        h3.innerText = `Common Name: ${country.name.common} Official Name: ${country.name.official}`;
+        console.log(country)
+        div.appendChild(h3);
+
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        div.appendChild(p);
+        countriesDiv.appendChild(div);
+    });
+}
